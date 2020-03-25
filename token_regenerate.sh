90f59f30bc53f12ed0226e495bb8a1ad69159f87
yuanren <reyren179@gmail.com>
2020-03-25 13:19:07 +0800

    publish token re-generate script
    Signed-off-by: yuanren <reyren179@gmail.com>

---
+++ token_regenerate.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+ADMIN_SECRET=$(kubectl get secrets -n kube-system | grep dashboard-admin | awk '{print $1}')
+DASHBOARD_LOGIN_TOKEN=$(kubectl describe secret -n kube-system ${ADMIN_SECRET} | grep -E '^token' | awk '{print $2}')                                                                        
+echo ${DASHBOARD_LOGIN_TOKEN}
